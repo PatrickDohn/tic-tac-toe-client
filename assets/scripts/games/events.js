8e48ab75717d2edc421640aba1e47ae232cb3f5d
PatrickDohn <dohnpatrick1993@gmail.com>
2020-07-21 22:33:56 -0400

    new branch and approach to project

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -3,63 +3,35 @@ const api = require('./api')
 const ui = require('./ui')
 const store = require('../store')
 
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-
 const onGameStart = function (event) {
+  // prevent refreshing the page everytime
   event.preventDefault()
   const form = event.target
   const formData = getFormFields(form)
-  store.player = 'X'
+  // assin the first piece to  the value X and store it in the store
+  // make requesst to API of new game starting
   api.gameStart(formData)
     .then(ui.gameStartSuccess)
     .catch(ui.gameStartFailure)
 }
 
-const switchPlayer = function () {
-  if (store.player === 'X') {
-    store.player = 'O'
-  } else if (store.player === 'O') {
-    store.player = 'X'
-  }
-}
-
-const onGamePosition = function (event) {
+const onGamePosition = function (clickEvent) {
   console.log(store.player)
-  const cell = event.target.id
-  // if cell value is not null make Api request if not alert cell full
-  console.log('you clicked' + cell)
-  event.preventDefault()
-  api.gamePosition(parseInt(cell) - 1)
-    .then(response => {
-      console.log('response object')
-      console.log(response.game.cells)
-      ui.clickPosition(cell, response)
-      whoWon(response.game.cells)
-      switchPlayer()
-    })
-}
-
-const whoWon = function (cells) {
-  if (cells[0, 1, 2] ===
-
+  const click = clickEvent.target
+  const index = $(click).data('cell-index')
+  store.index = index
+  store.clickedCell = clickEvent.target
+  console.log(index)
+  if (clickEvent.target.innerText === '') {
+    api.gamePosition(index, store.player)
+      .then(ui.clickPosition)
   }
 }
 
 module.exports = {
   onGameStart: onGameStart,
-  onGamePosition: onGamePosition,
-  winningCombos: winningCombos,
-  switchPlayer: switchPlayer
-
+  onGamePosition: onGamePosition
+  // switchPlayer: switchPlayer,
 }
 
 // use the store to hold what piece is being played. Use the game array to find patterns for winning positions. Verify position being filled.
--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -5,17 +5,43 @@ const gameStartSuccess = function (game) {
   store.game = game
 }
 
+let turn = true
 
+store.player = 'X'
 
-
-
-const clickPosition = function (id, game) {
-  console.log('You played X')
-  console.log(game)
-  store.game = game
-  $('#' + id).text(store.player)
-  console.log(id)
+const clickPosition = function (response) {
+  store.game = response.game
+  $(store.clickedCell).text(store.player)
+  const player = turn ? 'O' : 'X'
+  console.log(player)
+  turn = !turn
+  store.player = player
+  $('#playersTurn').text('Player ' + store.player)
+  // for loop to go through the array of winning combos
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < winningCombos.length; i++) {
+    // assign combos to winningCombos[i] so I dont have to retype as much
+    const combos = winningCombos[i]
+    console.log(response.game.cells[combos[0]] + response.game.cells[combos[1]] + response.game.cells[combos[2]])
+    // compare cells that have been clicked on to each index of winningCombos
+    if (response.game.cells[combos[0]] === response.game.cells[combos[1]] && response.game.cells[combos[1]] === response.game.cells[combos[2]] && response.game.cells[combos[2]] !== '') {
+      ($('#playersTurn').text('Player ' + store.player + ' wins!'))
+    }
+  }
 }
+
+// else if (response.game.cells[2] !== '' && response.game.cells[2] === response.game.cells[4] && response.game.cells[2] === response.game.cells[6]) {
+//     ($('#player-message').text('Player ' + response.game.cells[2] + ' wins!'))
+//   }
 
 module.exports = {
   gameStartSuccess: gameStartSuccess,
